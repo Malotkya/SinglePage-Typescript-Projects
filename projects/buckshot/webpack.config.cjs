4136bf84afcd4b87312cc655919fd4bf231b7e48
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-09 10:58:51 -0500

    Fixed webpack settings.

--- projects/buckshot/webpack.config.cjs
+++ projects/buckshot/webpack.config.cjs
@@ -1,5 +1,5 @@
 const {MultipleEntry} = require("../../webpack.cjs");
-const path = requrie("path");
+const path = require("path");
 
 const prod = process.argv.includes('prod');
 const source = path.join(__dirname, "src");
@@ -10,6 +10,6 @@ module.exports = MultipleEntry({
     style: path.join(source, "style.scss"),
     output: { 
         filename: '[name].js',
-        path: path.join(__dirname, "build")
+        path: path.join(__dirname, "public")
     }
 });
