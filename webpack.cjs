6f17a6fba72a9f368db9dddd26c0e2fa35ca88c2
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-09 10:04:26 -0500

    Config files for mono-repo

---
+++ webpack.cjs
@@ -0,0 +1,65 @@
+/** Main Webpack Config Options
+ * 
+ * @author Alex Malotky
+ */
+const fs = require("fs");
+const path = require("path");
+const json5 = require("json5");
+
+/** Get Aliases
+ * 
+ * @param {string} configPath
+ * @param {Record<string, string>} alias 
+ * @returns {Record<string, string>}
+ */
+function getAliases(configPath, alias = {}){
+    const {paths} = json5.parse(fs.readFileSync(configPath).toString()).compilerOptions;
+
+    const dir = path.normalize(configPath, "..")
+
+    for(const item in paths) {
+        const key = item.replace("/*", "");
+        const name = paths[item][0].replace("/*", "");
+        
+        alias[key] = path.resolve(dir, name);
+    }
+
+    return alias;
+}
+
+/** Single Entry
+ * 
+ * @param {{
+ *      mode: "development"|"production",
+ *      entry: string,
+ *      output: {
+ *          filename: string,
+ *          path: string
+ *      },
+ *      alias?: Record<string, string>
+ * }} opts 
+ * @returns {import("webpack").Configuration}
+ */
+function SingleEntry(opts){
+    const {mode, entry, output, alias} = opts
+    return {
+        mode, entry, output,
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+            alias: getAliases(path.join(__dirname, "tsconfig.json"), alias)
+        }
+    };
+}
+
+module.exports = {
+    SingleEntry, getAliases
+}
