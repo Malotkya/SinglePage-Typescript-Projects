9c9e964c03d06ddd1e411f3f3b2afadb352cfea9
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-10 09:31:52 -0500

    Streamlined App.

--- projects/terminal/src/Terminal/App.ts
+++ projects/terminal/src/Terminal/App.ts
@@ -1,55 +1,76 @@
-import System from "."
+/** /Terminal/App
+ * 
+ * @author Alex Malotky
+ */
 
-interface History{
-    list: Array<string>,
-    location: number;
-}
+
+export type MainFunction = (args:string[])=>Promise<void>|void;
+export type HelpFunction = ()=>Promise<void>|void;
 
 export default class App{
     private _call: string;
     private _description: string;
-
-    private _history: History;
+    private _history: string[];
+    private _location:number;
+    #main: MainFunction|undefined;
+    #help: HelpFunction|undefined;
 
     constructor(call: string, description: string){
         this._call = call.toLowerCase();
         this._description = description;
-
-        this._history = {
-            list: [],
-            location: -1
-        }
+        this._history = [];
+        this._location = -1;
     }
 
     public moveHistory(it: number){
-        if(this._history.list.length > 0) {
-            this._history.location += it;
+        if(this._history.length > 0) {
+            this._location += it;
 
-            if(this._history.location < 0) {
-                this._history.location = 0;
+            if(this._location < 0) {
+                this._location = 0;
             }
 
-            if(this._history.location >= this._history.list.length) {
-                this._history.location = this._history.list.length-1;
+            if(this._location >= this._history.length) {
+                this._location = this._history.length-1;
             }
 
-            return this._history.list[this._history.location];
+            return this._history[this._location];
         }
 
         return undefined;
     }
 
     protected addToHistory(s:string){
-        this._history.list.push(s);
-        this._history.location = this._history.list.length;
+        this._history.push(s);
+        this._location = this._history.length;
     }
 
-    public async main(system: System, args: Array<string>){
-        throw new Error("Main called in Bass App class!");
+    set main(value:MainFunction){
+        if(typeof value !== "function")
+            throw new TypeError("Main Function must be a function!");
+
+        this.#main = value;
     }
 
-    public help(system:System){
-        throw new Error("Help called in Bass App class!");
+    get main():MainFunction {
+        if(this.#main === undefined)
+            throw new Error(`Main funciton never set for ${this._call}!`);
+
+        return this.#main;
+    }
+
+    set help(value:HelpFunction){
+        if(typeof value !== "function")
+            throw new TypeError("Main Function must be a function!");
+        
+        this.#help = value;
+    }
+
+    get help():HelpFunction {
+        if(this.#help === undefined)
+            throw new Error(`Main funciton never set for ${this._call}!`);
+
+        return this.#help;
     }
 
     get call(){
