6f711a22de95a1aa4e8591394b81438c41766f5c
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-11 12:22:57 -0500

    Fixed issues with the clean up listener.

--- projects/terminal/src/Terminal/CleanUp.ts
+++ projects/terminal/src/Terminal/CleanUp.ts
@@ -11,9 +11,10 @@ const list:Destroyable[] = [];
 export function addToCleanup(item:Destroyable) {
     if(typeof item !== "object" || typeof item.destroy !== "function")
         throw new Error("Item is not destroyable!");
+    list.push(item);
 }
 
-window.addEventListener("close", async()=>{
+window.addEventListener("beforeunload", async(e)=>{
     for(const item of list){
         await item.destroy();
     }
