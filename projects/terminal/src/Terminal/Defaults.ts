3e932a0f735ac1ab9ab88c4622373c928a147355
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-11 14:53:09 -0500

    Use color classes.

--- projects/terminal/src/Terminal/Defaults.ts
+++ projects/terminal/src/Terminal/Defaults.ts
@@ -5,11 +5,12 @@
  * 
  * @author Alex Malotky
  */
+import Color from "@/Color";
 
 export const SCREEN_WIDTH = 100;
 export const SCREEN_HEIGHT = 30;
 
 export const FONT_SIZE = 15;
 
-export const COLOR_BACKGROUND = "black";
-export const COLOR_FONT = "green";
+export const COLOR_BACKGROUND = new Color(0, 0, 0);
+export const COLOR_FONT = new Color(0, 255, 0);
