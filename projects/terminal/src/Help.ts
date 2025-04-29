d5b8f27c75d316396a2a24755d4639e06e4ec22d
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-29 11:54:42 -0500

    Updated to match changes.

--- projects/terminal/src/Help.ts
+++ projects/terminal/src/Help.ts
@@ -1,27 +1,27 @@
-import System, {App} from "./System";
+import System, {App, Process} from "./System";
 
 class Help extends App {
     constructor(){
         super("help", "I try to help out how ever I can");
 
-        this.main = (args) => {
+        this.main = async(args) => {
             if(args[1] === undefined) {
 
                 for(const [name, app] of System) {
-                    if(app.description !== undefined)
+                    if(app.description)
                         System.println(`${name} - ${app.description}`);
                 }
             } else {
-                let app = null;
+                let app:Process|null = null;
                 let intrusive = (args[1] == "-i")
                 if(intrusive) {
-                    app = System.getApp(args[2].toLowerCase());
+                    app = System.getProcess(args[2].toLowerCase());
                 } else {
-                    app = System.getApp(args[1].toLowerCase());
+                    app = System.getProcess(args[1].toLowerCase());
                 }
     
                 if(typeof app?.help === "function") {
-                    app.help();
+                    await app.help();
                 } else {
                     System.println("Sorry, I don't know that application.");
                 }
