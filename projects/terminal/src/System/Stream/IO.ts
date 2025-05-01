0f7748dc6a9e8e8dd37e9c8b09ea62ea971d0b46
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-05-01 15:10:17 -0500

    Atempting to store stdIO in the filesystem.

--- projects/terminal/src/System/Stream/IO.ts
+++ projects/terminal/src/System/Stream/IO.ts
@@ -3,35 +3,76 @@
  * @author Alex Malotky
  */
 import {ReadStream, WriteStream, BufferReference} from "../Stream";
+import { writeToFile, createFile, readFile } from "../Files/Database";
+import { ROOT_USER_ID } from "../User";
+import Database from "../Database";
+
+const STDIN_FILE = "/sys/stdin";
+const STDOUT_FILE = "/sys/stdout";
+
+let input:string = "";
+let output:string = "";
+
+const ref = Database("FileSystem", "readwrite");
+ref.open().then(async(tx)=>{
+    try {
+        await createFile(STDIN_FILE, {recursive: true, soft: true, user: ROOT_USER_ID}, tx);
+        await createFile(STDOUT_FILE, {recursive: true, soft: true, user: ROOT_USER_ID}, tx);
+        output = await readFile(STDOUT_FILE, ROOT_USER_ID, tx as any);
+    } catch (e){
+        console.error(e)
+    }  finally {
+        ref.close();
+    }
+}).catch(e=>{
+    console.error(e);
+    ref.close();
+})
+
+
+
+async function updateInput () {
+    const ref = Database("FileSystem", "readwrite");
+    await writeToFile(STDIN_FILE, {user:ROOT_USER_ID, type: "Override"}, input, await ref.open());
+    ref.close();
+}
+
+async function udpateOutput () {
+    const ref = Database("FileSystem", "readwrite");
+    await writeToFile(STDIN_FILE, {user:ROOT_USER_ID, type: "Override"}, output, await ref.open());
+    ref.close();
+}
 
 let cursor:number = 0;
-let value:string = "";
+
 export const InputBuffer = {
     add(c:string){
-        value = value.substring(0, cursor) 
-            + c + value.substring(++cursor);
+        input = input.substring(0, cursor) 
+            + c + input.substring(++cursor);
+        updateInput();
     },
 
     delete() {
-        if(cursor < 0 || cursor > value.length)
+        if(cursor < 0 || cursor > input.length)
             return;
     
-        if(value.length > 0){
+        if(input.length > 0){
             if(cursor === 0){
-                value = value.substring(1);
+                input = input.substring(1);
             } else {
-                value = value.substring(0, cursor) + value.substring(cursor+1);
+                input = input.substring(0, cursor) + input.substring(cursor+1);
             }
         }
     },
 
     get value() {
-        return value;
+        return input;
     },
 
     set value(v:string) {
-        value = v;
+        input = v;
         cursor = v.length;
+        updateInput();
     },
 
     set cursor(value:number){
@@ -41,8 +82,8 @@ export const InputBuffer = {
     get cursor():number {
         if(cursor < 0)
             return 0;
-        else if(cursor > value.length)
-            return value.length;
+        else if(cursor > input.length)
+            return input.length;
         return cursor;
     }
 }
@@ -76,7 +117,15 @@ export class InputStream extends ReadStream {
 }
 
 
-export const OutputBuffer:BufferReference = {value:""};
+export const OutputBuffer:BufferReference = {
+    get value(){
+        return output;
+    },
+    set value(v:string){
+        output = v;
+        udpateOutput();
+    }
+};
 export class OutputStream extends WriteStream {
     constructor(){
         super(OutputBuffer);
